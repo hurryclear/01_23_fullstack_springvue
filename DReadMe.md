d22c7d09b4e4b4e7a628153b8dae9663cb71c403
hurryclear <hurjiangde@gmail.com>
2024-01-08 19:21:20 +0100

    change layout of menu bar

--- DReadMe.md
+++ DReadMe.md
@@ -7,5 +7,62 @@ But this is ONLY for personal usage, please don't forward without permission!
     - import Element in main.js with the following commands:
       - `import router from './router'`
       - `import ElementUI from 'element-ui';`
-      - `Vue.use(ElementUI);`
+      - `Vue.use(ElementUI, "small");`
 - Backend: 
+
+- 
+
+# Frontend
+
+## Change Layout
+
+- get code from Element->Layout Container and put that in HomeView.vue 
+
+- App.vue
+  ```vue
+  <template>
+    <div id="app">
+      <router-view/>
+    </div>
+  </template>
+  <style>
+  #app{
+    height: 100%;
+  }
+  </style>
+  ```
+
+- create global.css in ./src/assets
+  ```css
+  html, body, div {
+      margin: 0;
+      padding: 0;
+  }
+  
+  html, body {
+      height: 100%;
+  }
+  ```
+
+- import global.css in main.js
+  ```js
+  import Vue from 'vue'
+  import App from './App.vue'
+  import router from './router'
+  import ElementUI from 'element-ui';  
+  import 'element-ui/lib/theme-chalk/index.css';
+  import './assets/global.css'
+  
+  Vue.config.productionTip = false
+  Vue.use(ElementUI, "small");
+  
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+  
+  ```
+
+  
+
+- menu-folder (如何实现侧边栏收缩功能)
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,105 @@
 <template>
-  <div class="home">
-    <h1>{{msg}}</h1>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container style="min-height: 100vh">
+    <el-aside :width="sideWidth + 'px'" style="background-color: rgb(238, 241, 246); box-shadow: 2px 0 6px rgb(0 21 41 / 35%)">
+      <el-menu :default-openeds="[]"
+               style="min-height: 100%; overflow-x: hidden"
+               background-color="rgb(48, 65, 86)"
+               text-color="#fff"
+               active-text-color="#ffd04b"
+               :collapse-transition="false"
+               :collapse="isCollapse"
+      >
+<!--        header of left menu-->
+        <div style="height: 60px; line-height: 60px; text-align: center">
+          <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; margin-right: 5px">
+          <b style="color: #cccccc" v-show = "logoTextShow">Administration System</b>
+        </div>
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-message"></i>
+            <span slot="title">Navigator One</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">Group 1</template>
+            <el-menu-item index="1-1">Option 1</el-menu-item>
+            <el-menu-item index="1-2">Option 2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group 2">
+            <el-menu-item index="1-3">Option 3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">Option4</template>
+            <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-menu"></i>
+            <span slot="title">Navigator Two</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">Group 1</template>
+            <el-menu-item index="2-1">Option 1</el-menu-item>
+            <el-menu-item index="2-2">Option 2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group 2">
+            <el-menu-item index="2-3">Option 3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">Option 4</template>
+            <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Navigator Three</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">Group 1</template>
+            <el-menu-item index="3-1">Option 1</el-menu-item>
+            <el-menu-item index="3-2">Option 2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group 2">
+            <el-menu-item index="3-3">Option 3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="3-4">
+            <template slot="title">Option 4</template>
+            <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style="font-size: 12px; border-bottom: 1px solid #ccc; line-height: 60px; display: flex">
+        <div style="flex: 1; font-size: 20px">
+          <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
+        </div>
+
+        <el-dropdown style="width: 70px; cursor: pointer">
+          <span>Tom</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>Personal Information</el-dropdown-item>
+            <el-dropdown-item>Exist</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+
+      </el-header>
+
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="Date" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="Name" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="Address">
+          </el-table-column>
+        </el-table>
+      </el-main>
+
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,9 +112,33 @@ export default {
     HelloWorld
   },
   data () {
+    const item = {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles'
+    };
     return {
-      msg: "Hello World!"
+      tableData: Array(10).fill(item),
+      collapseBtnClass: 'el-icon-s-fold',
+      isCollapsed: false,
+      sideWidth: 200,
+      logoTextShow : true
+    }
+  },
+  methods: {
+    collapse() { //点击收缩按钮就会触发
+      this.isCollapsed = !this.isCollapsed
+      if(this.isCollapsed){ // fold
+        this.sideWidth = 64
+        this.collapseBtnClass = 'el-icon-s-unfold'
+        this.logoTextShow = false
+      } else { // unfold
+        this.sideWidth = 200
+        this.collapseBtnClass = 'el-icon-s-fold'
+        this.logoTextShow = true
+      }
     }
   }
 }
 </script>
+
