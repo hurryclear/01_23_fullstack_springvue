380653096e0980910e73cebc54cf02beaa8ed61f
hurryclear <hurjiangde@gmail.com>
2024-01-18 17:40:12 +0100

    cross port, fronted visit backend port

---
+++ backend/src/main/java/com/hurryclear/backend/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.hurryclear.backend.config;
+
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    private static final long MAC_AGE = 24 * 60 * 60;
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:8080");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setMaxAge(MAC_AGE);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
