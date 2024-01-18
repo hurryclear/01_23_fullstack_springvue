9ace55cd0e7b7ad36f661eb697e2130f44488232
hurryclear <hurjiangde@gmail.com>
2024-01-18 18:38:36 +0100

    hide password

--- backend/src/main/java/com/hurryclear/backend/entity/User.java
+++ backend/src/main/java/com/hurryclear/backend/entity/User.java
@@ -1,11 +1,13 @@
 package com.hurryclear.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data // provide getter&setter
 public class User {
     private Integer id;
     private String username;
+    @JsonIgnore
     private String password;
     private String nickname;
     private String email;
